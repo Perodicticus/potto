7f4c9a6caab8b1ef5861c75d997a12161bda66cb
Perodicticus <96774702+Perodicticus@users.noreply.github.com>
2022-01-13 15:59:55 +0100

    Add files via upload

---
+++ alene1.py
@@ -0,0 +1,23 @@
+
+
+print ("Hei, og velkommen til Ottos matteprogram.")
+print ("Her skal du først runde av tallene til nærmeste tier,")
+("og så løse mattestykket.")
+
+t = (5+5 == 20)
+f = False
+
+if (t == True):
+    print ("Dette klarte du jo kjempefint!")
+
+print ("5 + 5")
+    tall = input("Hva er svaret? ")
+    if int(tall) == 20:
+      return True
+    else:
+      return False
+
+
+if True: print ("Dette klarte du jo kjempefint!")
+
+if False: print ("Ikke helt riktig, husk å runde av til nærmeste tier!")
